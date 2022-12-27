f55f9cc3e6d1b0ef6a739af2e2fdde71da205dc1
Jeronimo luis Vera <jeronimo.l.vera@exxonmobil.com>
2022-12-27 14:18:35 -0300

    5

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,6 +9,9 @@ class Quiz extends Component {
         super(props);
         this.state = { quiz_position: 1 };
     }
+    showNextQuestion() {
+        this.setState({ quiz_position: this.state.quiz_position + 1 });
+    }
     render() {
         const isQuizEnd =
             this.state.quiz_position - 1 === quizData.quiz_questions.length;
@@ -23,6 +26,9 @@ class Quiz extends Component {
                                 this.state.quiz_position - 1
                             ]
                         }
+                        showNextQuestionHandler={this.showNextQuestion.bind(
+                            this
+                        )}
                     />
                 )}
             </div>
